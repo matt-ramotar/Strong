9db3578d71c8646d508a848c8036bb88f7a242bc
Matt Ramotar <matt.ramotar@jhu.edu>
2020-08-29 12:58:03 -0700

    Add Program migration

---
+++ backend/db/migrations/20200829195527-create-program.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Programs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      workoutId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'Workouts' },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Programs');
+  },
+};
