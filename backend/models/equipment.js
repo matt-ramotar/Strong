cc9c3303af08940e1d0b65b59621aee5995ac2b0
Matt Ramotar <matt.ramotar@jhu.edu>
2020-08-29 13:34:39 -0700

    Configure Equipment associations

--- backend/models/equipment.js
+++ backend/models/equipment.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Equipment extends Model {
     /**
@@ -10,14 +8,17 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Equipment.belongsTo(models.Exercise, { foreignKey: 'equipmentId' });
     }
-  };
-  Equipment.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Equipment',
-  });
+  }
+  Equipment.init(
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Equipment',
+    }
+  );
   return Equipment;
 };
