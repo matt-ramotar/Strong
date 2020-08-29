a82d74744c3bd2e9ab20d905047357d20ae2d080
Matt Ramotar <matt.ramotar@jhu.edu>
2020-08-29 13:25:55 -0700

    Update Exercise migration

---
+++ backend/db/migrations/20200829202314-create-exercise.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Exercises', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      instructions: {
+        type: Sequelize.TEXT,
+      },
+      typeId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'Exercise_Types' },
+      },
+      equipmentId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'Equipment' },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Exercises');
+  },
+};
