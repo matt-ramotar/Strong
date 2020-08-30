b50317848881d5003d3d2abcb4456c587f18a43f
Matt Ramotar <matt.ramotar@jhu.edu>
2020-08-29 17:54:47 -0700

    Add Equipment seeds

---
+++ backend/db/seeders/20200830003823-equipment.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkInsert('Equipment', [
+      { name: 'Bands', createdAt: new Date(), updatedAt: new Date() },
+      {
+        name: 'Foam Roll',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      { name: 'Barbell', createdAt: new Date(), updatedAt: new Date() },
+      {
+        name: 'Kettlebells',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Body Only',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      { name: 'Machine', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Cable', createdAt: new Date(), updatedAt: new Date() },
+      {
+        name: 'Medicine Ball',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Dumbbell',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      { name: 'None', createdAt: new Date(), updatedAt: new Date() },
+      {
+        name: 'E-Z Curl Bar',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      { name: 'Other', createdAt: new Date(), updatedAt: new Date() },
+      {
+        name: 'Exercise Ball',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkDelete('Equipment', null, {});
+  },
+};
