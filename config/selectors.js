e2b66638c6f13862facdf9eedf4cfc773eef0515
Matt Ramotar <matt.ramotar@jhu.edu>
2020-08-30 10:38:06 -0700

    Refactor

--- config/selectors.js
+++ config/selectors.js
@@ -1,11 +1,7 @@
 module.exports = {
   GetExercises: {
-    POPUP: 'body > div._381ROb8E-1-._4BjynQtD-1- > div > div > div.cII9UV0U-1- > div._4CT8xzMH-1- > a:nth-child(2)',
-    LOAD_MORE_BTN: '#js-ex-category-body > div.ExCategory-results > div.ExLoadMore > button',
-    RESET_FILTER_BTN: '#js-ex-category-body > div.ExCategory-results > p:nth-child(4) > button',
-    EXERCISE: '#js-ex-category-body > div.ExCategory-results > div:nth-child(INDEX-j) > div.ExResult-cell.ExResult-cell--nameEtc > h3 > a',
-    PRIMARY_MUSCLE:
-      '#js-ex-category-body > div.ExCategory-results > div:nth-child(INDEX-j) > div.ExResult-cell.ExResult-cell--nameEtc > div.ExResult-details.ExResult-muscleTargeted > a',
+    getName: '#js-ex-category-body > div.ExCategory-results > div:nth-child(INDEX-j) > div.ExResult-cell.ExResult-cell--nameEtc > h3 > a',
+    getEquipmentType:
+      '#js-ex-category-body > div.ExCategory-results > div:nth-child(INDEX-j) > div.ExResult-cell.ExResult-cell--nameEtc > div.ExResult-details.ExResult-equipmentType > a',
   },
-  acceptPrivacyPol: {},
 };
