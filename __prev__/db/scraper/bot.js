96340b4bc0300d5d024fcef701ff241b4a698e29
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-16 03:45:31 -0700

    Refactor

--- __prev__/db/scraper/bot.js
+++
@@ -1,5 +0,0 @@
-const axios = require('axios');
-
-const { Exercise } = require('./classes');
-
-const getExercises = async () => {};
--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, Migrate
 from .config import Config
 from .routes import index
+from .models import db
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(index.bp)
+db.init_app(app)
+migrate = Migrate(app, db)
