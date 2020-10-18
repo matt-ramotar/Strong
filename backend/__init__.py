b4856de8e735cc6dc215b6b122b546d5ca8c7c8f
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-18 09:25:00 -0700

    Configure CORS

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,19 +1,23 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_cors import CORS
 
 from .models import db, User
 from .config import Config
 from .routes import index, session, exercises
 
 app = Flask(__name__)
+cors = CORS(app)
 app.config.from_object(Config)
+
+
+db.init_app(app)
 
 app.register_blueprint(index.bp)
 app.register_blueprint(session.bp)
 app.register_blueprint(exercises.bp)
 
-db.init_app(app)
 migrate = Migrate(app, db)
 
 login = LoginManager(app)
--- backend/config.py
+++ backend/config.py
@@ -5,3 +5,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CORS_HEADERS = 'Content-Type'
