32dff6023c9b4ed2a4d12404c412b99e8657cbb2
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-24 15:37:16 -0700

    Set token expiration

--- app/config.py
+++ app/config.py
@@ -7,5 +7,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_HEADERS = 'Content-Type'
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = 604800
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
     JWT_BLACKLIST_ENABLED = True
